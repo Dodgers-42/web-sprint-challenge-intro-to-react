1f2740a5d982caca1eac41bf645d51f7ac2317f3
Dodger-42 <kevin.bing.barber@gmail.com>
2020-07-07 14:30:55 -0700

    first

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,12 +1,12 @@
 import React from "react";
 import styled from 'styled-components';
-
+import { Button } from 'reactstrap';
 const Character = (props) => {
     // console.log(props.starWarsPics);
     return (
 
         <Container className="starwarspics">
-            <h1>{props.character.name}</h1>
+            <Button>{props.character.name}</Button>
             <span>{props.character.height}</span>
             <span>{props.character.mass}</span>
             <p>{props.character.hair_color}</p>
@@ -20,8 +20,13 @@ const Character = (props) => {
 const Container = styled.div`
 display: flex;
 flex-direction: column;
+align-items: center;
 border: solid 2px;
 padding: 40px;
-    
+  button{
+      width: 200px;
+  }  
 `
+
+
 export default Character;
