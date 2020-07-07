f85431ab9e2d2090ecb4786b20c7370ff056be9f
Dodger-42 <kevin.bing.barber@gmail.com>
2020-07-06 17:49:10 -0700

    first

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -2,11 +2,15 @@ import React from "react";
 
 
 const Character = (props) => {
-    console.log(props.starWarsPics);
+    // console.log(props.starWarsPics);
     return (
 
         <div className="starwarspics">
-            {/* <name={props.character.name}/> */}
+            <h1>{props.character.name}</h1>
+            <span>{props.character.height}</span>
+            <span>{props.character.mass}</span>
+            <p>{props.character.hair_color}</p>
+            <p>{props.character.skin_color}</p>
 
 
         </div>
